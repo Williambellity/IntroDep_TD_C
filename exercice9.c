242b22c2b1438a24063a61c5549cef263b0b5699
William Bellity <william.bellity1@gmail.com>
2021-06-02 08:50:54 +0200

    eq str done

--- exercice9.c
+++ exercice9.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 
+#define TRUE 1
+#define FALSE 0
+
 int mylen(char *s);
+int eq_str(char *s1, char *s2);
 
 int main(void) {
     char s1[] = "test";
-    char s2[] = "testbis";
-    
-
+    char s2[] = "test";
+    int result = eq_str(s1, s2);
+    if (result == 1){
+        printf("True\n");
+    }
+    if (result == 0){
+        printf("False\n");
+    }
 }
 
 int mylen(char* s) {
@@ -22,13 +31,15 @@ int eq_str(char* s1,char* s2){
     int lens1 = mylen(s1);
     int lens2 = mylen(s2);
     if (lens1 != lens2) {
-        return 0
+        return 0;
     }
     int i = 0;
     while (i < lens1) {
-        if(s1[i] != s2[i]) {
+        i++;
+        if (s1[i] != s2[i])
+        {
             return 0;
         }
     }
-    return 1
+    return 1;
 }
