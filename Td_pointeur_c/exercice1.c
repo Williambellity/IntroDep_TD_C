3a748a2d6eef49b61250b93adf11f5c5bdc9436f
William Bellity <william.bellity1@gmail.com>
2021-06-03 11:00:30 +0200

    exercice 1 fully done

--- Td_pointeur_c/exercice1.c
+++ Td_pointeur_c/exercice1.c
@@ -1,22 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int putChaineInCharStar(char s[], char *ps, int lenght);
+void putChaineInCharStar(char s[], char *ps, int lenght);
 int mylen(char s[]);
+int nombreOccurence(char occ, char *s);
 
 int main(void) {
     char s[] = "Bonjour";
     int lenght = mylen(s);
     char *ps = malloc(lenght * sizeof(char));
     putChaineInCharStar(s, ps, lenght);
-    printf("ps = %s", ps);
+    printf("ps = %s\n", ps);
+    printf("Nombre d'occurence de o dans %s: %i\n", s, nombreOccurence('o', s));
 }
 
-int putChaineInCharStar(char s[], char *ps, int lenght) {
+void putChaineInCharStar(char s[], char *ps, int lenght) {
     for (int i = 0; i < lenght; i++) {
         *(ps + i) = s[i];
     }
 
+}
+
+int nombreOccurence(char occ, char* s) {
+    int lenght = mylen(s);
+    int compteur = 0;
+    for (int i = 0; i < lenght; i++)
+    {
+        if (s[i] == occ) {
+            compteur++;
+        }
+    }
+    return compteur;
 }
 
 int mylen(char s[]) {
